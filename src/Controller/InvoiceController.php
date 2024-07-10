8865613afe84e4e88d1e8736cafc6719611e3d31
Thierry Leung <ahkhiat@hotmail.com>
2024-07-10 23:32:00 +0200

    pdf

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,29 +10,57 @@ use Dompdf\Dompdf;
 
 class InvoiceController extends AbstractController
 {
-    #[Route('/facture', name: 'app_invoice')]
-    public function index(): Response
+    // Impression PDF pour utilisateur connecté
+    // vérification de la commande pour un utilisateur donné
+    #[Route('/compte/facture/impression/{id_order}', name: 'app_invoice_customer')]
+    public function invoiceCustomer(OrderRepository $orderRepository, $id_order): Response
     {
-        // instantiate and use the dompdf class
+        $order = $orderRepository->findOneById($id_order);
+
+        if(!$order) {
+            return $this->redirectToRoute('app_account');
+        }
+
+        if($order->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('app_account');
+        }
+
         $dompdf = new Dompdf();
-
-        $html = $this->renderView('invoice/index.html.twig');
+        $html = $this->renderView('invoice/index.html.twig', [
+            'order' => $order
+        ]);
         $dompdf->loadHtml($html);
-
-        // (Optional) Setup the paper size and orientation
         $dompdf->setPaper('A4', 'portait');
-
-        // Render the HTML as PDF
         $dompdf->render();
-
-        // Output the generated PDF to Browser
         $dompdf->stream('facture.pdf', [
             'Attachment' => false
         ]);
-
         exit();
-        // return $this->render('invoice/index.html.twig', [
-            
-        // ]);
+       
+    }
+
+    // Impression PDF pour admin
+    // vérification de la commande pour un utilisateur donné
+    #[Route('/admin/facture/impression/{id_order}', name: 'app_invoice_admin')]
+    public function invoiceAdmin(OrderRepository $orderRepository, $id_order): Response
+    {
+        $order = $orderRepository->findOneById($id_order);
+
+        if(!$order) {
+            return $this->redirectToRoute('admin');
+        }
+
+        $dompdf = new Dompdf();
+        $html = $this->renderView('invoice/index.html.twig', [
+            'order' => $order
+        ]);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portait');
+        $dompdf->render();
+        $dompdf->stream('facture.pdf', [
+            'Attachment' => false
+        ]);
+        exit();
+       
     }
 }
